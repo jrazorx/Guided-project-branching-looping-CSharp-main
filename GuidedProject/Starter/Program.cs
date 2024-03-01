361ef54ece6e0b6c37736a882bd92c109210218f
jra <julien.gault.50@gmail.com>
2024-03-01 15:32:16 +0100

    Option 2 : Count the number of pets in the ourAnimals array

--- GuidedProject/Starter/Program.cs
+++ GuidedProject/Starter/Program.cs
@@ -126,7 +126,15 @@ do
 
         case "2":
             // Add a new animal friend to the ourAnimals array
-            Console.WriteLine("this app feature is coming soon - please check back to see progress.");
+            string anotherPet = "y";
+            int petCount = 0;
+            for (int i = 0; i < maxPets; i++)
+            {
+                if (ourAnimals[i, 0] != "ID #: ")
+                {
+                    petCount ++;
+                }
+            }
             Console.WriteLine("Press the Enter key to continue.");
             readResult = Console.ReadLine();
             break;
