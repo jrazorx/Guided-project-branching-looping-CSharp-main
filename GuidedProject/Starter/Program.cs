b9897dda1c52073b45b7a1e265a03147e489ff34
jra <julien.gault.50@gmail.com>
2024-03-04 17:33:29 +0100

    Option 2 : Save the new pet information

--- GuidedProject/Starter/Program.cs
+++ GuidedProject/Starter/Program.cs
@@ -224,6 +224,14 @@ do
                         }
                     } while (animalPersonalityDescription == "");
 
+                    // store the pet information in the ourAnimals array (zero based)
+                    ourAnimals[petCount, 0] = "ID #: " + animalID;
+                    ourAnimals[petCount, 1] = "Species: " + animalSpecies;
+                    ourAnimals[petCount, 2] = "Age: " + animalAge;
+                    ourAnimals[petCount, 3] = "Nickname: " + animalNickname;
+                    ourAnimals[petCount, 4] = "Physical description: " + animalPhysicalDescription;
+                    ourAnimals[petCount, 5] = "Personality: " + animalPersonalityDescription;
+
                     // get the pet's nickname. animalNickname can be blank.
                     do
                     {
