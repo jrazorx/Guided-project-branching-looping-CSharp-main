571fff380895357dedcb57f5fcd9a309ba12ca3e
jra <julien.gault.50@gmail.com>
2024-03-04 17:33:01 +0100

    Option 2 : Build loop to read and validate the pet's nickname

--- GuidedProject/Starter/Program.cs
+++ GuidedProject/Starter/Program.cs
@@ -224,6 +224,24 @@ do
                         }
                     } while (animalPersonalityDescription == "");
 
+                    // get the pet's nickname. animalNickname can be blank.
+                    do
+                    {
+                        Console.WriteLine("Enter a nickname for the pet");
+                        readResult = Console.ReadLine();
+
+                        if (readResult != null)
+                        {
+                            animalNickname = readResult.ToLower();
+
+                            if (animalNickname == "")
+                            {
+                                animalNickname = "tbd";
+                            }
+
+                        }
+                    } while (animalNickname == "");
+
                     // increment petCount (the array is zero-based, so we increment the counter after adding to the array)
                     petCount++;
                     // check maxPet limit
